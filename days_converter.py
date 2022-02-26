7bb99d224095451f44bf295d3e9faea45a650a05
AGGPD <andypd01@yahoo.com>
2022-02-26 16:25:05 +1100

    Merge remote-tracking branch 'origin/master'

--- days_converter.py
+++ days_converter.py
@@ -5,10 +5,10 @@ user_input = ""
 while user_input != "exit":
     user_input = input(user_input_message)
     days_and_unit = user_input.split(":")
-    print(days_and_unit)
+    # print(days_and_unit)
     days_and_unit_dict = {"days":days_and_unit[0], "unit":days_and_unit[1]}
-    print(days_and_unit_dict)
-    print(type(days_and_unit_dict))
+    # print(days_and_unit_dict)
+    # print(type(days_and_unit_dict))
     validate_and_execute(days_and_unit_dict)
 
 
